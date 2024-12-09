da2f3eb2fe1a923067ee4de3400618a023aecfa0
horvathnauzika <naihorvath@gmail.com>
2024-12-09 09:41:32 +0100

    f1 4 meghivasa javitas

--- src/main/java/hu/szamalk/Main.java
+++ src/main/java/hu/szamalk/Main.java
@@ -3,6 +3,6 @@ package hu.szamalk;
 public class Main {
     public static void main(String[] args) {
         Tábla tábla=new Tábla('#');
-        tábla.kirajzol(8,8);
+        tábla.Megjelenit(8,8);
     }
 }
