dd43813b75a3c47fa3abb37c6045a4015ea3cf15
horvathnauzika <naihorvath@gmail.com>
2024-12-09 09:36:37 +0100

    f1 3

--- src/main/java/hu/szamalk/Main.java
+++ src/main/java/hu/szamalk/Main.java
@@ -3,6 +3,5 @@ package hu.szamalk;
 public class Main {
     public static void main(String[] args) {
 
-
     }
 }
--- src/main/java/hu/szamalk/Tábla.java
+++ src/main/java/hu/szamalk/Tábla.java
@@ -1,6 +1,21 @@
 package hu.szamalk;
 
 public class Tábla {
-    private char[][] T;
+    private char T[][] ;
     private char ÜresCella;
+
+    public Tábla(char[][] t) {
+        this.T = new char[8][8];
+    }
+
+    public Tábla(char üresCella){
+        this.ÜresCella=üresCella;
+    }
+
+    public Tábla(char[][] t, char üresCella) {
+        this.ÜresCella = üresCella;
+        this.T = new char[üresCella][üresCella];
+    }
+
+
 }
