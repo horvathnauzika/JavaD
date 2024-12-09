c54f7dc5e6b52da0cf6dce5435e4711a91bd1f64
horvathnauzika <naihorvath@gmail.com>
2024-12-09 10:11:32 +0100

    f1 9

--- src/main/java/hu/szamalk/Main.java
+++ src/main/java/hu/szamalk/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Tábla tábla=new Tábla('#');
         tábla.Megjelenit(8,8);
-
+        System.out.println("9. feladat: Üres oszlopok és sorok száma:");
+        System.out.println("Oszlopok: " + tábla.ÜresOszlopokSzáma() + "\n" + "Sorok: "+tábla.ÜresSorokSzáma());
     }
 }
